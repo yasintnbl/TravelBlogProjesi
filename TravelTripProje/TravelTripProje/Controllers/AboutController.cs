a52f0fd433f5bc8980528b356a5572be39bb3e95
yasintnbl <yasintnbl@gmail.com>
2021-04-24 17:34:58 +0300

    TravelBlog
    Seyahat Blogu(İlk Mvc)

---
+++ TravelTripProje/TravelTripProje/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkımızdas.ToList();
+
+            return View(degerler);
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Controllers/IletisimController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+        Context c = new Context();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(İletisim i)
+        {
+            c.İletisims.Add(i);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+
+    }
+}
